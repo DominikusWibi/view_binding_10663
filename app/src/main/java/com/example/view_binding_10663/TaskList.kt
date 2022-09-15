e1becc3bd1ddf2513a3fcd70e763d73893156d96
DominikusWibi <wibidanang456@gmail.com>
2022-09-15 08:09:13 +0700

    Initial commit

---
+++ app/src/main/java/com/example/view_binding_10663/TaskList.kt
@@ -0,0 +1,11 @@
+package com.example.view_binding_10663
+
+object TaskList {
+    val TaskList = listOf<Task>(
+        Task("Chapter 1","Kuliah","Biar Pintar"),
+        Task("Chapter 2","Main basket","Karena gabut"),
+        Task("Chapter 3","Makan","Karena lapar"),
+        Task("Chapter 4","Tidur","Karena capek"),
+        Task("Chapter 5","Main game","Karena bosan")
+    )
+}
